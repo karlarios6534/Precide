4741abd9f3df3b0d648ca919be2e41a7d393b3b3
karlarios6534 <karla.rios6534@alumnos.udg.mx>
2023-02-04 21:08:57 -0600

    Datatables added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.datatables.net/1.13.2/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="{{ asset ('css/styles.css') }}"/>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/dataTables.bootstrap5.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.3.4/css/buttons.bootstrap5.min.css"/>
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
--- resources/views/patient/index.blade.php
+++ resources/views/patient/index.blade.php
@@ -1,12 +1,16 @@
+
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Patients') }}
         </h2>
         <a href = "patients/create" class="btn btn-info"><img src="{{URL::asset('img/plus.png')}}" alt="create"width = "10rem"></a>
+       
     </x-slot>
-    <div class = "content" style="margin-left : 3rem; margin-right : 3rem " >
-    <table class = "table table-success table-striped mt-4">
+
+    <div class = "content" style="margin-left : 3rem; margin-right : 3rem "  >
+    <table id="patients" class = "table table-striped mt-4" style="width:100%" cellspacing="0">
         <thead>
             <tr>
                 <th scope="col">Id</th>
@@ -37,4 +41,50 @@
         </tbody>
     </table>
 </div>
+
+<!-- ScriptJS to use datatables properties -->
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.2/js/dataTables.bootstrap5.min.js"></script>
+<!-- ScriptJS to use datatables buttons properties -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.13.2/js/dataTables.bootstrap5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.4/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.4/js/buttons.bootstrap5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.4/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.4/js/buttons.print.min.js"></script>
+<!--ScriptJS to use iconns from fontawesome-->
+<script src="https://kit.fontawesome.com/0395bf88e1.js" crossorigin="anonymous"></script>
+<!--ScriptJS to make datatable-->
+<script>
+$(document).ready(function () {
+    $('#patients').DataTable({
+        "lengthMenu" : [[5,10,50,-1],[5,10,50,"All"]],
+        "responsive" : "true",
+        "dom" : 'Bfrtilp',
+        "buttons" : [
+            {
+                extend: 'excelHtml5',
+                text: '<i class="fas fa-file-excel "></i>',
+                titleAttr: 'Export to excel'
+            },
+            {
+                extend: 'pdfHtml5',
+                text: '<i class="fas fa-file-pdf"></i>',
+                titleAttr: 'Export to pdf'
+            },
+            {
+                extend: 'print',
+                text: '<i class="fa fa-print" ></i>',
+                titleAttr: 'Print'
+            }
+            
+        ]
+    });
+});
+</script>
+
 </x-app-layout>
